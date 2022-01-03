ceba5bd67e4e4270a01c19c582b611841599d766
dak2 <dawt2h@gmail.com>
2022-01-04 00:23:26 +0900

    add saving model

---
+++ db/migrate/20220103152022_create_savings.rb
@@ -0,0 +1,11 @@
+class CreateSavings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :savings do |t|
+      t.string :label,   null: false, default: ""
+      t.integer :money,  null: false, default: 0
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
