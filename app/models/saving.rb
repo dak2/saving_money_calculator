56e8ca10f2f2b6962d5531f48b8a435d0faf7e25
dak2 <dawt2h@gmail.com>
2022-01-04 16:51:43 +0900

    exec annotate

--- app/models/saving.rb
+++ app/models/saving.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: savings
+#
+#  id         :integer          not null, primary key
+#  label      :string           default(""), not null
+#  money      :integer          default("0"), not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Saving < ApplicationRecord
   belongs_to :user
 end
