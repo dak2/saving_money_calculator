0d5567a729bcf6cd05661c37418744f6939763ea
dak2 <dawt2h@gmail.com>
2022-01-04 17:18:31 +0900

    add savings controller, views

--- db/migrate/20220104081233_create_savings.rb
+++ db/migrate/20220104081233_create_savings.rb
@@ -1,8 +1,8 @@
 class CreateSavings < ActiveRecord::Migration[7.0]
   def change
     create_table :savings do |t|
-      t.string :label,   null: false, default: ""
-      t.integer :money,  null: false, default: 0
+      t.string :label,    null: false, default: ""
+      t.integer :money,   null: false, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
