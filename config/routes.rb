6b6f4bc72f331d1e51abefe4e596768d86c747ee
dak2 <dawt2h@gmail.com>
2022-04-24 19:56:24 +0900

    usersコントローラーなど追加 rails g controller Users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
