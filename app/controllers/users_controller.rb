25facc4d1e2b34606e6f2b4d33ab6a67089534bc
dak2 <dawt2h@gmail.com>
2022-05-09 12:53:53 +0900

    カテゴリーの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,14 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
     @savings = @user.savings.page(params[:page])
     @savings_top_three = @user.savings.order(money: :desc).limit(3)
+    # delegate to decorator
+    savings_hash = @user.savings.left_joins(:category)
+                                .group('categories.label')
+                                .count
+                                .map do |k,v|
+                                  k = 'その他' if k.nil?
+                                  { k => v }
+                                end
+    @savings_by_categories = {}.merge(*savings_hash)
   end
 end
