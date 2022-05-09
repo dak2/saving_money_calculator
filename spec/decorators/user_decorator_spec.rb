9b93b59d9c952e1b567a36bbe5842fed5503fc1b
dak2 <dawt2h@gmail.com>
2022-05-09 15:36:05 +0900

    rubocop

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UserDecorator, type: :decorator do
   let(:user) { create(:user) }
   before { ActiveDecorator::Decorator.instance.decorate user }
-  let!(:savings) { create_list(:saving, 2, :with_category, user: user) }
+  let!(:savings) { create_list(:saving, 2, :with_category, user:) }
   subject { user.savings_by_categories }
 
   describe '#savings_by_categories' do
@@ -13,13 +13,13 @@ describe UserDecorator, type: :decorator do
       end
 
       it 'return hash key "その他" instead of nil' do
-        expect(subject).to eq ({'category_1'=>1, 'その他'=>1})
+        expect(subject).to eq({ 'category_1' => 1, 'その他' => 1 })
       end
     end
 
     context 'categories#labelがnilが存在しない場合' do
       it 'return hash key as they are' do
-        expect(subject).to eq ({'category_3'=>1, 'category_4'=>1})
+        expect(subject).to eq({ 'category_3' => 1, 'category_4' => 1 })
       end
     end
   end
