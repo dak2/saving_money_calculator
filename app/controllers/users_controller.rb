da7fa41ae65c01fc329d1210b815ea5788c2b5a6
dak2 <dawt2h@gmail.com>
2022-05-21 23:49:25 +0900

    検索ボックス設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :search_savings, only: [:show]
 
   def show
-    @user = User.find(current_user.id)
-    @savings = @user.savings.includes(:category).page(params[:page])
-    @savings_top_three = @user.savings.includes(:category).order(money: :desc).limit(3)
+    @savings = @q.result.page(params[:page])
+  end
+
+  private
+
+  def search_savings
+    @q = current_user.savings.includes(:category).ransack(params[:q])
+    @q.sorts = 'id desc' if @q.sorts.empty?
   end
 end
