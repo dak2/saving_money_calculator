91c936fd0787363bce1f60b4a35b06bb7ac7e575
dak2 <dawt2h@gmail.com>
2022-06-05 21:10:30 +0900

    rubocop適用

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
 pin 'application'
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin 'chartkick', to: 'chartkick.js'
 pin 'Chart.bundle', to: 'Chart.bundle.js'
